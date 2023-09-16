e5aca6a7f2ed461692c7e4aa5391efb033746941
KokiizGreat <128624869+KokiX333@users.noreply.github.com>
2023-09-15 21:45:42 -0700

    Update time&Date.pas

--- time&Date.pas
+++ time&Date.pas
@@ -1,7 +1,11 @@
 program Time&Date
 uses sysutils;
-
+var
+   YY,MM,DD : Word;
+   
 begin
 writeIn ('Current time:',TimeToStr(Time));
-writeIn ('Current date:',DateToStr(Date));
-end
+writeln ('Date : ',Date);
+   DeCodeDate (Date,YY,MM,DD);
+   writeln (format ('Today is (DD/MM/YY): %d/%d/%d ',[dd,mm,yy]));
+   end
